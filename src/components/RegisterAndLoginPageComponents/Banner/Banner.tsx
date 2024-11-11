d30e660ae492a2fa43aa0313f2d32011f3ef431e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-11 16:23:58 +0300

    add banner for mobile devices

--- src/components/RegisterAndLoginPageComponents/Banner/Banner.tsx
+++ src/components/RegisterAndLoginPageComponents/Banner/Banner.tsx
@@ -1,7 +1,30 @@
 import css from "./Banner.module.css";
 
 const Banner = () => {
-  return <div className={css["image-container"]}>BANNER</div>;
+  return (
+    <div className={css["image-container"]}>
+      <picture>
+        <source
+          srcSet="
+            /images/desktop/home-page-image-1x.png 1x,
+            /images/desktop/home-page-image-2x.png 2x
+          "
+          media="(min-width: 1440px)"
+        />
+        <source
+          srcSet="
+         /images/mobile/home-page-image-1x.png 1x,
+         /images/mobile/home-page-image-2x.png 2x
+        "
+          media="(min-width: 320px)"
+        />
+        <img
+          src="/images/mobile/home-page-image-1x.png"
+          alt="home page image a boy and a girl reading a book and studying together"
+        />
+      </picture>
+    </div>
+  );
 };
 
 export default Banner;
