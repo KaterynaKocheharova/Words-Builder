d46355ae866badd4b69bffaa344a326afbae6934
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-11 15:08:28 +0300

    add restricted route to use for login and registration pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
+import { useEffect, lazy, Suspense } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import RestrictedRoute from "./RestrictedRoute";
+import PrivateRoute from "./PrivateRoute";
+import { refreshUser } from "./redux/auth/operations";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const RegistrationPage = lazy(() =>
+  import("./pages/RegistrationPage/RegistrationPage")
+);
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <div id="App">
-      <p>Register</p>
+      <Suspense fallback={null}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  component={<RegistrationPage />}
+                  redirectTo={"/contacts"}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  component={<LoginPage />}
+                  redirectTo={"/contacts"}
+                />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute
+                  component={<ContactsPage />}
+                  redirectTo="/login"
+                />
+              }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Layout>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/RestrictedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+
+type RestrictedRouteProps = {
+  component: React.ReactNode;
+  redirectTo: string;
+};
+
+const RestrictedRoute = ({
+  component: Component,
+  redirectTo,
+}: RestrictedRouteProps) => {
+  //   const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  //   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+  return Component;
+};
+
+export default RestrictedRoute;
