ab7c63d1f10f0aedf3504bf8f947995b9c5e8ae6
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-12 17:02:20 +0300

    add registration-container to registration-page

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -2,14 +2,17 @@ import Container from "../../components/CommonComponents/Container/Container";
 import Section from "../../components/CommonComponents/Section/Section";
 import Banner from "../../components/RegisterAndLoginPageComponents/Banner/Banner";
 import RegisterForm from "../../components/RegisterAndLoginPageComponents/RegisterForm/RegisterForm";
+import css from "./RegistrationPage.module.css";
 
 const RegistrationPage = () => {
   return (
     <Section sectionClass="auth-section">
-      <Container>
-        <Banner />
-      </Container>
-      <RegisterForm />
+      <div className={css["registration-container"]}>
+        <Container>
+          <Banner />
+        </Container>
+        <RegisterForm />
+      </div>
     </Section>
   );
 };
