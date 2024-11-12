9d000b67ac252b0b5f54bbaf345e459d97c080b8
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-12 21:55:08 +0300

    style 1440 version of registration page

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Section from "../../components/CommonComponents/Section/Section";
 import Banner from "../../components/RegisterAndLoginPageComponents/Banner/Banner";
 import RegisterForm from "../../components/RegisterAndLoginPageComponents/RegisterForm/RegisterForm";
@@ -5,13 +6,36 @@ import KeyWords from "../../components/RegisterAndLoginPageComponents/KeyWords/K
 import css from "./RegistrationPage.module.css";
 
 const RegistrationPage = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1400);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1400);
+    };
+    window.addEventListener("resize", handleResize);
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Section sectionClass="auth-section">
-      <div className={css["registration-container"]}>
-        <Banner />
-        <RegisterForm />
-      </div>
-      <KeyWords />
+      {isLargeScreen ? (
+        <div className={css["registration-container"]}>
+          <div className={css["banner-keywords-container"]}>
+            <Banner />
+            <KeyWords />
+          </div>
+          <RegisterForm />
+        </div>
+      ) : (
+        <>
+          <div className={css["registration-container"]}>
+            <Banner />
+            <RegisterForm />
+          </div>
+          <KeyWords />
+        </>
+      )}
     </Section>
   );
 };
