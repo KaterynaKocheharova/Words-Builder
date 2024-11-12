f3124aa5c128f8db19846e052c3ed50e4c0168c2
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-12 14:48:12 +0300

    add different input border colors to different states

--- src/components/CommonComponents/Input/Input.tsx
+++ src/components/CommonComponents/Input/Input.tsx
@@ -2,13 +2,16 @@ import { InputHTMLAttributes } from "react";
 import css from "./Input.module.css";
 import clsx from "clsx";
 
-type InputProps = { extraClass: string } & Partial<
+type InputProps = { extraClass: string; error?: boolean } & Partial<
   InputHTMLAttributes<HTMLInputElement>
 >;
 
-const Input = ({ extraClass, ...props }: InputProps) => {
+const Input = ({ extraClass, error, ...props }: InputProps) => {
   return (
-    <input className={clsx(css.input, extraClass && css[extraClass])} {...props} />
+    <input
+      className={clsx(css.input, extraClass && css[extraClass], error && css["wrong-input"])}
+      {...props}
+    />
   );
 };
 
