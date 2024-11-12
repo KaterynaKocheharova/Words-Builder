7221866df5d20695fc5a184f4c42927c857e5a46
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-12 15:32:41 +0300

    add toggle password btn to password input component

--- src/components/RegisterAndLoginPageComponents/PasswordInput/PasswordInput.tsx
+++ src/components/RegisterAndLoginPageComponents/PasswordInput/PasswordInput.tsx
@@ -1,21 +1,36 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useState } from "react";
 import Relative from "../../CommonComponents/Relative/Relative";
 import Input from "../../CommonComponents/Input/Input";
 import InputError from "../../CommonComponents/InputError/InputError";
+import css from "./PasswordInput.module.css";
 
 type PasswordInputProps = {
   errorMessage?: string;
 } & Partial<InputHTMLAttributes<HTMLInputElement>>;
 
 const PasswordInput = ({ errorMessage, ...props }: PasswordInputProps) => {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
   return (
     <Relative>
       <Input
-        type="password"
+        type={isPasswordShown ? "text" : "password"}
         placeholder="Password"
         extraClass="auth-form-input"
         {...props}
       />
+      <button
+        onClick={() => setIsPasswordShown(!isPasswordShown)}
+        className={css["eye-button"]}
+      >
+        <svg width="20" height="20" className={css["eye-icon"]}>
+          <use
+            href={`/sprite.svg#${
+              isPasswordShown ? "icon-eye" : "icon-eye-off"
+            }`}
+          ></use>
+        </svg>
+      </button>
       {errorMessage && <InputError errorMessage={errorMessage} />}
     </Relative>
   );
