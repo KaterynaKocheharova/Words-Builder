c7c1b37e9582581d7a6299c8273fdddb452174fa
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-14 16:00:57 +0300

    simplify consitional rendering in registration page

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -8,11 +8,15 @@ import css from "./RegistrationPage.module.css";
 const RegistrationPage = () => {
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1400);
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 767);
+  const [isMediumScreen, setIsMediumScreen] = useState(
+    window.innerWidth >= 768 && window.innerWidth <= 1440
+  );
 
   useEffect(() => {
     const handleResize = () => {
       setIsLargeScreen(window.innerWidth >= 1400);
       setIsSmallScreen(window.innerWidth <= 767);
+      setIsMediumScreen(window.innerWidth >= 768 && window.innerWidth <= 1439);
     };
     window.addEventListener("resize", handleResize);
 
@@ -21,23 +25,14 @@ const RegistrationPage = () => {
 
   return (
     <Section sectionClass="auth-section">
-      {isLargeScreen ? (
-        <div className={css["registration-container"]}>
-          <div className={css["banner-keywords-container"]}>
-            <Banner />
-            <KeyWords />
-          </div>
-          <RegisterForm />
+      <div className={css["registration-container"]}>
+        <div className={css["banner-keywords-container"]}>
+          <Banner />
+          {isLargeScreen && <KeyWords />}
         </div>
-      ) : (
-        <>
-          <div className={css["registration-container"]}>
-            <Banner />
-            <RegisterForm />
-          </div>
-          {!isSmallScreen && <KeyWords />}
-        </>
-      )}
+        <RegisterForm />
+      </div>
+      {isMediumScreen  && !isLargeScreen && <KeyWords />}
     </Section>
   );
 };
