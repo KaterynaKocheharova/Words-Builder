c688692f9f262721dff1dc56183c797bb8228c3f
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-14 15:44:55 +0300

    adjust styles for medium screen of login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Section from "../../components/CommonComponents/Section/Section";
 import Banner from "../../components/RegisterAndLoginPageComponents/Banner/Banner";
 import LoginForm from "../../components/RegisterAndLoginPageComponents/LoginForm/LoginForm";
@@ -5,17 +6,48 @@ import KeyWords from "../../components/RegisterAndLoginPageComponents/KeyWords/K
 import css from "./LoginPage.module.css";
 
 const LoginPage = () => {
-  return (
-    <Section sectionClass="auth-section">
-      <div className={css["login-container"]}>
-        <div className={css["banner-keywords-container"]}>
-          <Banner />
-          <KeyWords />
-        </div>
-        <LoginForm />
-      </div>
-    </Section>
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1400);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 767);
+  const [isMediumScreen, setIsMediumScreen] = useState(
+    window.innerWidth >= 768 && window.innerWidth <= 1440
   );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1400);
+      setIsSmallScreen(window.innerWidth <= 767);
+      setIsMediumScreen(window.innerWidth >= 768 && window.innerWidth <= 1440);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (isSmallScreen) {
+    return (
+      <Section sectionClass="auth-section">
+        <div className={css["login-container"]}>
+          <div className={css["banner-keywords-container"]}>
+            <Banner />
+            <KeyWords />
+          </div>
+          <LoginForm />
+        </div>
+      </Section>
+    );
+  }
+
+  if (isMediumScreen) {
+    return (
+      <Section sectionClass="auth-section">
+        <div className={css["login-container"]}>
+          <Banner />
+          <LoginForm />
+        </div>
+        <KeyWords />
+      </Section>
+    );
+  }
 };
 
 export default LoginPage;
