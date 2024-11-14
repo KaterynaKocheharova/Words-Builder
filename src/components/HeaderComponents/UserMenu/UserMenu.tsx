dc67bcc8149e5125568a56ed89522d91a87b7ff3
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-15 02:29:12 +0300

    add logout button to the user menu

--- src/components/HeaderComponents/UserMenu/UserMenu.tsx
+++ src/components/HeaderComponents/UserMenu/UserMenu.tsx
@@ -1,10 +1,12 @@
 import Avatar from "../Avatar/Avatar";
+import LogoutButton from "../LogoutButton/LogoutButton";
 import css from "./UserMenu.module.css";
 
 const UserMenu = () => {
   return (
     <div className={css.flex}>
       <Avatar />
+      <LogoutButton />
     </div>
   );
 };
