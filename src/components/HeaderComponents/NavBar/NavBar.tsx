889d628d04692d08802ddba73b507b13457f5dd7
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-15 01:47:22 +0300

    render navigation and user menu only for desktop

--- src/components/HeaderComponents/NavBar/NavBar.tsx
+++ src/components/HeaderComponents/NavBar/NavBar.tsx
@@ -1,11 +1,14 @@
 import { selectIsLoggedIn } from "../../../redux/auth/selectors";
 import { useAppSelector } from "../../../redux/hooks";
+import { useScreenSizes } from "../../../hooks/useScreenSizes";
 import Container from "../../CommonComponents/Container/Container";
 import Logo from "../Logo/Logo";
 import NavLinks from "../NavLinks/NavLinks";
+import UserMenu from "../UserMenu/UserMenu";
 import css from "./NavBar.module.css";
 
 const NavBar = () => {
+  const { isLargeScreen } = useScreenSizes();
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
 
   return (
@@ -13,7 +16,12 @@ const NavBar = () => {
       <Container>
         <div className={css.flex}>
           <Logo />
-          {isLoggedIn && <NavLinks />}
+          {isLoggedIn && isLargeScreen && (
+            <>
+              <NavLinks />
+              <UserMenu />
+            </>
+          )}
         </div>
       </Container>
     </header>
---
+++ src/components/HeaderComponents/UserMenu/UserMenu.tsx
@@ -0,0 +1,12 @@
+import css from "./UserMenu.module.css";
+
+
+const UserMenu = () => {
+  return (
+    <div>
+      USER MENU
+    </div>
+  )
+}
+
+export default UserMenu;
