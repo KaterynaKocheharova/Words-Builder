9e9c1142d0bafafed8b8934641a28f885ec97b7c
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-15 02:27:59 +0300

    add and style logout button

---
+++ src/components/HeaderComponents/LogoutButton/LogoutButton.tsx
@@ -0,0 +1,14 @@
+import css from "./LogoutButton.module.css";
+
+const LogoutButton = () => {
+  return (
+    <button className={css["logout-button"]}>
+      Log out
+      <svg width="16" height="16">
+        <use href="/sprite.svg#icon-arrow-right"></use>
+      </svg>
+    </button>
+  );
+};
+
+export default LogoutButton;
