a8b2f532235e18b9699c5302a2220ae7e253d87b
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-15 16:48:38 +0300

    finish logout feature

--- src/components/HeaderComponents/LogoutButton/LogoutButton.tsx
+++ src/components/HeaderComponents/LogoutButton/LogoutButton.tsx
@@ -1,8 +1,22 @@
+import { useAppDispatch } from "../../../redux/hooks";
+import { logoutUser } from "../../../redux/auth/operations";
+import { toast } from "react-toastify";
 import css from "./LogoutButton.module.css";
 
 const LogoutButton = () => {
+  const dispatch = useAppDispatch();
+
+  const handleLogoutClick = () => {
+    dispatch(logoutUser())
+      .unwrap()
+      .then(() => {
+        toast.success("Logout successfull");
+      })
+      .catch((error: string) => toast.error(error));
+  };
+
   return (
-    <button className={css["logout-button"]}>
+    <button onClick={handleLogoutClick} className={css["logout-button"]}>
       Log out
       <svg width="16" height="16">
         <use href="/sprite.svg#icon-arrow-logout"></use>
