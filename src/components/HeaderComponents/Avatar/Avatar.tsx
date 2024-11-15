e2a34ffe75276d504af33cc29dba66f881173ce8
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-15 17:30:49 +0300

    fix avatar colors - now they are the same for all screens

--- src/components/HeaderComponents/Avatar/Avatar.tsx
+++ src/components/HeaderComponents/Avatar/Avatar.tsx
@@ -1,14 +1,19 @@
 import { useAppSelector } from "../../../redux/hooks";
 import { selectUsername } from "../../../redux/auth/selectors";
+import clsx from "clsx";
 import css from "./Avatar.module.css";
 
-const Avatar = () => {
+type AvatarProps = {
+  extraRoundClass: string;
+};
+
+const Avatar = ({ extraRoundClass }: AvatarProps) => {
   const userName = useAppSelector(selectUsername);
 
   return (
     <div className={css.flex}>
       <p className={css.name}>{userName}</p>
-      <div className={css["avatar-round"]}>
+      <div className={clsx(css["avatar-round"], css[extraRoundClass])}>
         <svg width="24" height="24" className={css["avatar-icon"]}>
           <use href="/sprite.svg#icon-user"></use>
         </svg>
