6b9a6da3ab7d75c425842480e9321e1185134d42
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-16 23:43:48 +0300

    make select smaller for tablet + screen sizes

--- src/components/DictionaryPageComponents/Dashboard/AddWordForm/AddWordForm.tsx
+++ src/components/DictionaryPageComponents/Dashboard/AddWordForm/AddWordForm.tsx
@@ -54,79 +54,87 @@ const AddWordForm = () => {
     console.log(data);
   };
 
-  //   const DropdownIndicator = (
-  //     props: DropdownIndicatorProps<Option[], false>
-  //   ) => {
-  //     return (
-  //       <components.DropdownIndicator {...props}>
-  //         <button className={css["dropdown-indicator"]}>
-  //           <svg width="20" height="20" className={css["dropdown-icon"]}>
-  //             <use href="/sprite.svg#icon-dropdown"></use>
-  //           </svg>
-  //         </button>
-  //       </components.DropdownIndicator>
-  //     );
-  //   };
+  const DropdownIndicator = (
+    props: DropdownIndicatorProps<Option[], false>
+  ) => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <button>
+          <svg width="20" height="20" className={css["dropdown-icon"]}>
+            <use href="/sprite.svg#icon-dropdown"></use>
+          </svg>
+        </button>
+      </components.DropdownIndicator>
+    );
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="category"
-        control={control}
-        render={({ field }) => (
-          <Select
-            unstyled
-            {...field}
-            options={[
-              { value: "chocolate", label: "Chocolate" },
-              { value: "strawberry", label: "Strawberry" },
-              { value: "vanilla", label: "Vanilla" },
-            ]}
-            styles={{
-              control: (baseStyles) => ({
-                position: "relative",
-                backgroundColor: "transparent",
-                border: "1px solid #D1D5DB",
-                borderRadius: "15px",
-                padding: "12px 24px",
-                fontWeight: "500",
-                lineHeight: "1.5",
-                color: "var(--white)",
-                cursor: "pointer",
-              }),
-              dropdownIndicator: (baseStyles) => ({
-                position: "absolute",
-                top: "14px",
-                right: "14px",
-                stroke: "#121417",
-                fill: "none",
-                cursor: "pointer",
-              }),
-              menu: (baseStyles) => ({
-                ...baseStyles,
-                backgroundColor: "#FFFFFF",
-                borderRadius: "14px",
-                padding: "14px 18px",
-                marginTop: "4px",
-                display: "flex",
-                flexDirection: "column",
-                columnGap: "8px",
-              }),
-              option: (baseStyles, state) => ({
-                fontSize: "18px",
-                fontWeight: "500",
-                lineHeight: "1.1",
-                color: state.isSelected ? "#121417" : "#12141733",
-                marginBottom: "8px",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                cursor: "pointer",
-              }),
-            }}
-          />
-        )}
-      />
+      <div className={css["select-wrapper"]}>
+        <Controller
+          name="category"
+          control={control}
+          render={({ field }) => (
+            <Select
+              unstyled
+              {...field}
+              options={[
+                { value: "chocolate", label: "Chocolate" },
+                { value: "strawberry", label: "Strawberry" },
+                { value: "vanilla", label: "Vanilla" },
+              ]}
+              // container 32% starting from 768
+              styles={{
+                control: (baseStyles) => ({
+                  position: "relative",
+                  backgroundColor: "transparent",
+                  border: "1px solid #FCFCFC4D",
+                  borderRadius: "15px",
+                  padding: "12px 24px",
+                  fontWeight: "500",
+                  lineHeight: "1.5",
+                  color: "var(--white)",
+                  cursor: "pointer",
+                }),
+                dropdownIndicator: (baseStyles) => ({
+                  ...baseStyles,
+                  position: "absolute",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                  width: "20px",
+                  height: "20px",
+                  right: "24px",
+                  stroke: "var(--white)",
+                  fill: "none",
+                  cursor: "pointer",
+                }),
+                menu: (baseStyles) => ({
+                  ...baseStyles,
+                  backgroundColor: "#FFFFFF",
+                  borderRadius: "14px",
+                  padding: "14px 18px",
+                  marginTop: "4px",
+                  display: "flex",
+                  flexDirection: "column",
+                  columnGap: "8px",
+                }),
+                option: (baseStyles, state) => ({
+                  fontSize: "18px",
+                  fontWeight: "500",
+                  lineHeight: "1.1",
+                  color: state.isSelected ? "#121417" : "#12141733",
+                  marginBottom: "8px",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  cursor: "pointer",
+                }),
+              }}
+            />
+          )}
+        />
+      </div>
+
       {/* <p>
         {errors?.category?.value?.message || errors?.category?.label?.message}
       </p> */}
