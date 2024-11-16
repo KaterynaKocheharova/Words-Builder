713be36efc56677689d3dc64d3df8f1d0d686a97
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-16 16:18:46 +0300

    add and style modal close btn

--- src/components/CommonComponents/ModalWrapper/ModalWrapper.tsx
+++ src/components/CommonComponents/ModalWrapper/ModalWrapper.tsx
@@ -19,6 +19,11 @@ const ModalWrapper = ({
     <Dialog open={isOpen} onClose={close}>
       <DialogBackdrop className={css.backdrop}>
         <DialogPanel className={clsx(css.content, css[extraClass])}>
+          <button className={css["close-button"]} onClick={close}>
+            <svg width="24" height="24" className={css["close-icon"]}>
+              <use href="/sprite.svg#icon-close"></use>
+            </svg>
+          </button>
           {children}
         </DialogPanel>
       </DialogBackdrop>
