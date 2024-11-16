7571625a3aa3b22f43e0b5fd3e6484eea421f94e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-16 15:30:34 +0300

    Add and style AddWordButton

--- src/components/DictionaryPageComponents/Dashboard/AddWordButton/AddWordButton.tsx
+++ src/components/DictionaryPageComponents/Dashboard/AddWordButton/AddWordButton.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import css from "./AddWordButton.module.css";
 
 const AddWordButton = () => {
-  return <button>ADD WORD</button>;
+  return (
+    <button className={css.button}>
+      Add word
+      <svg width="20" height="20" className={css["add-icon"]}>
+        <use href="/sprite.svg#icon-plus"></use>
+      </svg>
+    </button>
+  );
 };
 
 export default AddWordButton;
