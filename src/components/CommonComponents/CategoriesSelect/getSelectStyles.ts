38d9fedd910dafc99ef0da09360d50979fec0c06
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-17 14:51:49 +0300

    make a reusable categories select

---
+++ src/components/CommonComponents/CategoriesSelect/getSelectStyles.ts
@@ -0,0 +1,63 @@
+import { StylesConfig } from "react-select";
+import { type Option } from "./CategoriesSelect";
+
+type GetSelectStyles = (location: string) => StylesConfig<Option>;
+
+export const getSelectStyles: GetSelectStyles = (location) => {
+  return {
+    control: (baseStyles) => ({
+      backgroundColor: "transparent",
+      border: "1px solid #FCFCFC4D",
+      borderRadius: "15px",
+      padding: "12px 24px",
+      fontWeight: "500",
+      lineHeight: "1.5",
+      color: "var(--white)",
+      cursor: "pointer",
+    }),
+    dropdownIndicator: (baseStyles) => ({
+      ...baseStyles,
+      position: "absolute",
+      top: "50%",
+      transform: "translateY(-50%)",
+      width: "20px",
+      height: "20px",
+      right: "24px",
+      stroke: "var(--white)",
+      fill: "none",
+      cursor: "pointer",
+    }),
+    menu: (baseStyles) => ({
+      ...baseStyles,
+      maxHeight: "240x",
+      backgroundColor: "#FFFFFF",
+      borderRadius: "15px",
+      padding: "12px 6px 12px 24px",
+      marginTop: "4px",
+      boxSizing: "border-box",
+    }),
+    menuList: (baseStyles) => ({
+      maxHeight: "216px",
+      overflowY: "auto",
+      "::-webkit-scrollbar": {
+        width: "8px",
+        height: "42%",
+        cursor: "pointer",
+      },
+      "::-webkit-scrollbar-thumb": {
+        background: "#F8F8F8",
+        borderRadius: "12px",
+      },
+      "::-webkit-scrollbar-track": {
+        background: "transparent",
+      },
+    }),
+    option: (baseStyles, state) => ({
+      fontSize: "16px",
+      fontWeight: 500,
+      lineHeight: 1.5,
+      color: state.isSelected ? "var(--primary-color)" : "#12141780",
+      cursor: "pointer",
+    }),
+  };
+};
--- src/components/DictionaryPageComponents/Dashboard/AddWordForm/AddWordForm.tsx
+++ src/components/DictionaryPageComponents/Dashboard/AddWordForm/AddWordForm.tsx
@@ -1,13 +1,10 @@
 import Select, { components, DropdownIndicatorProps } from "react-select";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { type Option } from "../../../CommonComponents/CategoriesSelect/CategoriesSelect";
+import CategoriesSelect from "../../../CommonComponents/CategoriesSelect/CategoriesSelect";
 import * as yup from "yup";
 import css from "./AddWord.module.css";
-
-type Option = {
-  label: string;
-  value: string;
-};
 
 export type AddWordFormValues = {
   category: { label: string; value: string };
@@ -54,20 +51,6 @@ const AddWordForm = () => {
     console.log(data);
   };
 
-  const DropdownIndicator = (
-    props: DropdownIndicatorProps<Option[], false>
-  ) => {
-    return (
-      <components.DropdownIndicator {...props}>
-        <button>
-          <svg width="20" height="20" className={css["dropdown-icon"]}>
-            <use href="/sprite.svg#icon-dropdown"></use>
-          </svg>
-        </button>
-      </components.DropdownIndicator>
-    );
-  };
-
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className={css["select-wrapper"]}>
@@ -75,102 +58,14 @@ const AddWordForm = () => {
           name="category"
           control={control}
           render={({ field }) => (
-            <Select
-              menuIsOpen={true}
-              unstyled
-              {...field}
-              options={[
-                { value: "chocolate", label: "Chocolate" },
-                { value: "strawberry", label: "Strawberry" },
-                { value: "vanilla", label: "Vanilla" },
-                { value: "mint", label: "Mint" },
-                { value: "caramel", label: "Caramel" },
-                { value: "coffee", label: "Coffee" },
-                { value: "hazelnut", label: "Hazelnut" },
-                { value: "banana", label: "Banana" },
-                { value: "peach", label: "Peach" },
-                { value: "mango", label: "Mango" },
-                { value: "blueberry", label: "Blueberry" },
-                { value: "raspberry", label: "Raspberry" },
-                { value: "pistachio", label: "Pistachio" },
-                { value: "lemon", label: "Lemon" },
-                { value: "lime", label: "Lime" },
-                { value: "coconut", label: "Coconut" },
-                { value: "cherry", label: "Cherry" },
-                { value: "watermelon", label: "Watermelon" },
-                { value: "grape", label: "Grape" },
-                { value: "apple", label: "Apple" },
-              ]}
-              styles={{
-                control: (baseStyles) => ({
-                  backgroundColor: "transparent",
-                  border: "1px solid #FCFCFC4D",
-                  borderRadius: "15px",
-                  padding: "12px 24px",
-                  fontWeight: "500",
-                  lineHeight: "1.5",
-                  color: "var(--white)",
-                  cursor: "pointer",
-                }),
-                dropdownIndicator: (baseStyles) => ({
-                  ...baseStyles,
-                  position: "absolute",
-                  top: "50%",
-                  transform: "translateY(-50%)",
-                  width: "20px",
-                  height: "20px",
-                  right: "24px",
-                  stroke: "var(--white)",
-                  fill: "none",
-                  cursor: "pointer",
-                }),
-                menu: (baseStyles) => ({
-                  ...baseStyles,
-                  maxHeight: "240x",
-                  backgroundColor: "#FFFFFF",
-                  borderRadius: "15px",
-                  padding: "12px 6px 12px 24px",
-                  marginTop: "4px",
-                  boxSizing: "border-box",
-                }),
-                menuList: (baseStyles) => ({
-                  maxHeight: "216px",
-                  overflowY: "auto",
-                  "::-webkit-scrollbar": {
-                    width: "8px",
-                    height: "42%",
-                    cursor: "pointer",
-                  },
-                  "::-webkit-scrollbar-thumb": {
-                    background: "#F8F8F8",
-                    borderRadius: "12px",
-                  },
-                  "::-webkit-scrollbar-track": {
-                    background: "transparent",
-                  },
-                }),
-                option: (baseStyles, state) => ({
-                  fontSize: "16px",
-                  fontWeight: 500,
-                  lineHeight: 1.5,
-                  color: state.isSelected
-                    ? "var(--primary-color)"
-                    : "#12141780",
-                  cursor: "pointer",
-                }),
-              }}
-              classNames={{
-                option: () => css["custom-option"],
-                menu: () => css["custom-select-menu"],
-              }}
-            />
+            <CategoriesSelect location="form" {...field} />
           )}
         />
       </div>
 
-      {/* <p>
+      <p>
         {errors?.category?.value?.message || errors?.category?.label?.message}
-      </p> */}
+      </p>
 
       <button type="submit">Submit</button>
     </form>
