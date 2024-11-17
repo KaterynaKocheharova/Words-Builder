7deb8896abefdc9d4240346b8f2c32fb57af044d
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-17 19:56:55 +0300

    unmount the verb type radios if the category is not verb

--- src/components/DictionaryPageComponents/Dashboard/AddWordForm/AddWordForm.tsx
+++ src/components/DictionaryPageComponents/Dashboard/AddWordForm/AddWordForm.tsx
@@ -1,87 +1,5 @@
-// import { useEffect, useRef } from "react";
-// import { useForm, Controller, SubmitHandler } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import CategoriesSelect, {
-//   Option,
-// } from "../../../CommonComponents/CategoriesSelect/CategoriesSelect";
-// import * as yup from "yup";
-// import Relative from "../../../CommonComponents/Relative/Relative";
-// import InputError from "../../../CommonComponents/InputError/InputError";
-// import css from "./AddWord.module.css";
-// import VerbTypeRadio from "./VerbTypeRadio/VerbTypeRadio";
-
-// export type AddWordFormValues = {
-//   category: Option;
-// };
-
-// const defaultValues: AddWordFormValues = {
-//   category: { label: "", value: "" },
-// };
-
-// const addWordFormSchema = yup.object({
-//   category: yup.object({
-//     label: yup.string().required("Category is required"),
-//     value: yup.string().required("Category is required"),
-//   }),
-// });
-
-// const AddWordForm = () => {
-//   const {
-//     handleSubmit,
-//     formState: { errors },
-//     control,
-//     setFocus,
-//   } = useForm<AddWordFormValues>({
-//     defaultValues,
-//     resolver: yupResolver(addWordFormSchema),
-//   });
-
-//   const categorySelectRef = useRef<any>(null);
-
-//   useEffect(() => {
-//     if (categorySelectRef.current) {
-//       categorySelectRef.current.focus();
-//     }
-//   }, []);
-
-//   const onSubmit: SubmitHandler<AddWordFormValues> = (data) => {
-//     console.log(data);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <div className={css["select-wrapper"]}>
-//         <Relative>
-//           <Controller
-//             name="category"
-//             control={control}
-//             render={({ field }) => (
-//               <CategoriesSelect
-//                 location="form"
-//                 {...field}
-//                 ref={categorySelectRef}
-//               />
-//             )}
-//           />
-
-//           <InputError
-//             errorMessage={
-//               errors?.category?.value?.message ||
-//               errors?.category?.label?.message
-//             }
-//           />
-//         </Relative>
-//       </div>
-//       <VerbTypeRadio />
-//       <button type="submit">Submit</button>
-//     </form>
-//   );
-// };
-
-// export default AddWordForm;
-
 import { useEffect, useRef } from "react";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { useForm, Controller, SubmitHandler, useWatch } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import CategoriesSelect, {
   Option,
@@ -115,13 +33,18 @@ const AddWordForm = () => {
     handleSubmit,
     formState: { errors },
     control,
-    setFocus,
   } = useForm<AddWordFormValues>({
     defaultValues,
+    shouldUnregister: true,
     resolver: yupResolver(addWordFormSchema),
   });
 
   const categorySelectRef = useRef<any>(null);
+
+  const categoryValue = useWatch({
+    control,
+    name: "category.value",
+  });
 
   useEffect(() => {
     if (categorySelectRef.current) {
@@ -156,13 +79,21 @@ const AddWordForm = () => {
           />
         </Relative>
       </div>
-      <Controller
-        name="verbType"
-        control={control}
-        render={({ field }) => (
-          <VerbTypeRadio value={field.value} onChange={field.onChange} />
+
+      <Relative>
+        {" "}
+        {categoryValue === "verb" && (
+          <Controller
+            name="verbType"
+            control={control}
+            render={({ field }) => (
+              <VerbTypeRadio value={field.value} onChange={field.onChange} />
+            )}
+          />
         )}
-      />
+        <InputError errorMessage={errors?.verbType?.message} />
+      </Relative>
+
       <button type="submit">Submit</button>
     </form>
   );
